92cb37210aace58506a3fb616697b5ed805cecf6
BilalRASLEN <eng.bll.raslen@gmail.com>
2023-03-06 04:35:41 +0300

    Create Professor.java Entity

--- src/main/java/com/blluniversity/entities/Professor.java
+++ src/main/java/com/blluniversity/entities/Professor.java
@@ -1,4 +1,54 @@
 package com.blluniversity.entities;
 
+
+import jakarta.persistence.*;
+
+@Entity(name = "Professor")
+@Table(
+        name = "Professor",
+        uniqueConstraints =@UniqueConstraint(
+                name = "email_unique",
+                columnNames = "email"
+        ) )
 public class Professor {
+
+    @Id
+    @SequenceGenerator(
+            name = "professor_sequence",
+            sequenceName = "professor_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "professor_sequence"
+    )
+    @Column(
+            name = "id",
+            nullable = false,
+            length = 11,
+            updatable = false,
+            unique = true
+    )
+    private Long id;
+    @Column(
+            name = "first_name",
+            nullable = false,
+            length = 255,
+            columnDefinition = "TEXT"
+    )
+    private String firstName;
+    @Column(
+            name = "last_name",
+            nullable = false,
+            length = 255,
+            columnDefinition = "TEXT"
+    )
+    private String lastName;
+    @Column(
+            name = "email",
+            length = 255,
+            columnDefinition = "TEXT"
+    )
+    private String email;
+
 }
