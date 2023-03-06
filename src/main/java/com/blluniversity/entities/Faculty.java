2a69dbdf0dcf641b14ae7406ee8bcb66467d9127
BilalRASLEN <eng.bll.raslen@gmail.com>
2023-03-06 04:16:38 +0300

    Created Faculty.java class

--- src/main/java/com/blluniversity/entities/Faculty.java
+++ src/main/java/com/blluniversity/entities/Faculty.java
@@ -1,11 +1,31 @@
 package com.blluniversity.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity(name = "Faculty")
 @Table(name = "Faculty")
 public class Faculty {
+    @Id
+    @SequenceGenerator(
+            name = "faculty_sequence",
+            sequenceName = "faculty_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "faculty_sequence"
+    )
+    @Column(
+            name = "id",
+            nullable = false,
+            length = 11,
+            updatable = false,
+            unique = true
+    )
+    private Long id;
+
+
+
 
 
 }
