8589815f243daa48897c7960ce81138dc8edf5d3
BilalRASLEN <eng.bll.raslen@gmail.com>
2023-03-06 04:31:42 +0300

    Create Course.java Entity

--- src/main/java/com/blluniversity/entities/Course.java
+++ src/main/java/com/blluniversity/entities/Course.java
@@ -1,4 +1,65 @@
 package com.blluniversity.entities;
 
+
+import jakarta.persistence.*;
+
+@Entity(name = "Course")
+@Table(name = "Course")
 public class Course {
+    @Id
+    @SequenceGenerator(
+            name = "course_sequence",
+            sequenceName = "course_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "course_sequence"
+    )
+    @Column(
+            name = "id",
+            nullable = false,
+            length = 11,
+            updatable = false,
+            unique = true
+    )
+    private Long id;
+    @Column(
+            name = "course_name",
+            nullable = false,
+            length = 255,
+            columnDefinition = "TEXT"
+    )
+    private String courseName;
+
+    public Course(String courseName) {
+        this.courseName = courseName;
+    }
+    public Course(){
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "id=" + id +
+                ", courseName='" + courseName + '\'' +
+                '}';
+    }
 }
